87f10ca86c8ed44a5a9a538efac55c0289d54054
dear <dear@fresnoU.org>
2023-09-14 09:16:46 -0700

    Madlibs

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res,) {
   let body = req.body;
   let newStory = getStory(body);
   res.render('story', {
@@ -10,24 +10,24 @@ router.get('/', function(req, res, next) {
   });
 })
  function getStory(formData) {
-   if(formData.storyChoice === "1") {
+   if (formData.storyChoice === "1") {
      return generateStory1(formData);
-   }else if (formData.storyChoice === "2") {
+   } else if (formData.storyChoice === "2") {
      return generateStory2(formData);
    }else if (formData.storyChoice === "3") {
      return generateStory3(formData);
-   }else {
+   } else {
      return "invalid";
    }
  }
  function generateStory1(formData){
-   return 'Twas a dark and stormy ${formData.noun}. The ${formData.adjective1} pumpkins started to ${formData.verb1}!'
+   return `Twas a dark and stormy night ${formData.noun1}. The ${formData.adjective1} pumpkins started to ${formData.verb1}!`
  }
 function generateStory2(formData){
-  return 'Twas a cold and wintery ${formData.noun1}. The ${formData.adjective1} pumpkins started to ${formData.verb1}!'
+  return `Twas a cold and wintery ${formData.noun1}. The ${formData.adjective1} pumpkins started to ${formData.verb1}!`
 }
 function generateStory3(formData){
-  return 'Twas a rainy and cold ${formData.noun1}. The ${formData.adjective1} pumpkins started to ${formData.verb1}!'
+  return `Twas a rainy and cold ${formData.noun1}. The ${formData.adjective1} pumpkins started to ${formData.verb1}!`
 }
 
 module.exports = router;
